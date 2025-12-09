ac40ae1a86fb44b0b4d80402b55c4253d213e6a1
Alexis Sanmiguel <83426426+alexisstdev@users.noreply.github.com>
2025-12-08 21:39:52 -0600

    Add entrypoint script to set up directories and permissions

---
+++ entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+# Crear estructura de carpetas si no existe
+mkdir -p /var/www/html/storage/framework/cache
+mkdir -p /var/www/html/storage/framework/sessions
+mkdir -p /var/www/html/storage/framework/views
+mkdir -p /var/www/html/storage/logs
+
+# Dar permisos totales (ya que somos root)
+chmod -R 777 /var/www/html/storage
+
+# Iniciar la aplicación
+exec /init
